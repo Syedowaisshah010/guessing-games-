54ee0acb042724321d398d34454e93064b3d5aa2
Syedowaisshah010 <159150128+Syedowaisshah010@users.noreply.github.com>
2024-03-25 15:49:34 +0500

    Add files via upload

---
+++ guessing.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// let guessing_number=12
+let guessing_number = Math.floor(Math.random() * 6 + 1);
+let guessing = await inquirer.prompt([
+    {
+        message: "please  Guessing number between 1-6 ",
+        type: "number",
+        name: "Guessing_number",
+    },
+]);
+if (guessing_number == guessing.Guessing_number) {
+    console.log("congratulation");
+}
+else {
+    console.log("sorry you guess wrong number");
+}
